c97872e016e48a9ff406c376890740e38a9ac381
Jiun Hong Chan <jiunhong@hotmail.com>
2024-12-24 01:22:23 +1100

    add path

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import sys
+import os
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(f'parent dir {parent_dir}')
+sys.path.insert(0, parent_dir)
+
 import logging
 import Pages
 from WebEngine import dash_engine
+import os 
+
+parent_dir = os.path.dirname(os.getcwd())
 
 pages = []
 pages.append(Pages.HomePage())
